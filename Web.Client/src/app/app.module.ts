3b09d675429d54608d46bfad5e27e69bf4aeea37
Michael Murphy <michael.w.murphy@outlook.com>
2022-05-14 09:53:29 +0100

    Initial implementation of Angular UI

---
+++ Web.Client/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ShareModule } from './share/share.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ShareModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
