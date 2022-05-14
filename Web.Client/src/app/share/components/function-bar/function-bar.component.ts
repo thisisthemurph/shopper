97831a6da8791028db73b831ed7bd8f0fbfd8a32
Michael Murphy <michael.w.murphy@outlook.com>
2022-05-14 10:55:58 +0100

    Implements header and function-bar component

---
+++ Web.Client/src/app/share/components/function-bar/function-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-function-bar',
+  templateUrl: './function-bar.component.html',
+  styleUrls: ['./function-bar.component.scss']
+})
+export class FunctionBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Web.Client/src/app/share/share.module.ts
+++ Web.Client/src/app/share/share.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { FunctionBarComponent } from './components/function-bar/function-bar.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FunctionBarComponent],
   imports: [CommonModule],
   exports: [HeaderComponent],
 })
