fd97cf06fb91c2ef3f2ed30146015cc3c685414e
Michael Murphy <michael.w.murphy@outlook.com>
2022-05-14 18:08:18 +0100

    Shows number of items in list

--- Web.Client/src/app/shoppinglist/models/shoppinglist.interface.ts
+++ Web.Client/src/app/shoppinglist/models/shoppinglist.interface.ts
@@ -1,9 +1,17 @@
+export interface ShoppingListItem {
+  id: number;
+  listId: number;
+  name: string;
+  category: null;
+  status: number;
+}
+
 export interface ShoppingList {
   id: number;
   name: string;
   description: string;
   settings: string | null;
-  items: null;
+  items: ShoppingListItem[];
   createdAt: string;
   updatedAt: string;
 }
