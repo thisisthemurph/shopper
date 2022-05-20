c8611918cb568701d016e859bedf06fd6e60178c
Michael Murphy <michael.w.murphy@outlook.com>
2022-05-20 06:28:51 +0100

    Cleans up Program.cs by implementing builder.Services extension methods

--- Shopper.Api/Extensions/ServiceExtensions.cs
+++ Shopper.Api/Extensions/ServiceExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 using System.Text;
 
 namespace Shopper.Api.Extensions
@@ -21,5 +23,37 @@ namespace Shopper.Api.Extensions
                 });
         }
 
+        public static string ConfigureCors(this IServiceCollection services) {
+            var AllowSpecificOrigins = "allowSpecificOrigins";
+            
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: AllowSpecificOrigins, policy =>
+                {
+                    policy.WithOrigins("http://localhost:4200");
+                    policy.AllowAnyHeader();
+                });
+            });
+
+            return AllowSpecificOrigins;
+        }
+
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
+        }
     }
 }
